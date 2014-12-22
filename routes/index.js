14130dc82f7a8e86bc947d6ab7db325d14a38026
Stefan Schinkel <stefan.schinkel@gmail.com>
2014-12-22 16:25:26 +0100

    Working data input and query.

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,30 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express'),
+    router = express.Router(),
+    redis = require("redis"),
+    client = redis.createClient();
 
+
+client.on("error", function (err) {
+    console.log("error event - " + client.host + ":" + client.port + " - " + err);
+});
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'nodeNews', loglevel: req.app.get('debug')});
+
+
+    // get all items of newsItems
+    client.lrange('newsItems', 0, -1, function (error, items) {
+        if (error){
+            console.log("error@" + client.host + ":" + client.port 
+                + " - " + err);
+            throw error;
+        }
+        res.render('index', 
+                    { title: 'nodeNews', 
+                    loglevel: req.app.get('debug'),
+                    newsItems: items.reverse(),
+                    });
+   }); // query callback
 });
 
 module.exports = router;
--- routes/news.js
+++ routes/news.js
@@ -1,18 +1,25 @@
 var express = require('express');
-var router = express.Router();
+var router = express.Router(),
+    redis = require("redis"),
+    client = redis.createClient();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('news', { title: 'News input' });
+    res.render('news', { title: 'News input' });
 });
 
 
 /* POST sth the page */
 router.post('/', function(req,res){
-	console.log('Posted to news was:');
-	console.log(req.body)
-    res.end('It worked!');
+    storeString = req.body.content;
+    console.log('Posted to news was: ' + storeString);
+    client.lpush('newsItems',storeString);
+    res.status(200).end();
 });
+   
+
+
+
 
 module.exports = router;
 
